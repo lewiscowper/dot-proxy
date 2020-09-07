958604b386f99d315d45f2f8f1fb21e6bb780105
Lewis Cowper <lewis@lewiscowper.com>
2020-09-07 23:28:48 +0200

    We have a DNS - DNSoT proxy, connecting to a hard coded address, and entirely dying if that is unavailable, but all this can be iterated upon

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ DNS to DNS over TLS proxy
 
 ---
 
-## Commit History
+## Commit History (latest first)
 
-- This commit takes in a basic dns server with https://godoc.org/github.com/miekg/dns. It responds to queries from kdig over tcp, (and udp is but a goroutine away). Next steps will be taking what I have now and forwarding the requests over tls to an external dns server.
+- Looks like we have a DNS to DNS over TLS proxy, after another few hours today. I'll add a udp server too, just to make my kdig query shorter, and tomorrow can be reserved for packaging and documenting usage and how to test. What I'm still not too sure about is how exactly to verify that the connection is being made, aside from the fact that the previous commit wouldn't respond with anything useful nameserver or DNS record wise, and this commit does. That means I'm definitely connecting to cloudflare's DNS servers generally, but I'm not 100% sure how to validate that it's going over DoT, other than I'm hitting 1.1.1.1:853. Something to think about in the documentation I guess
+- This commit takes in a basic DNS server with https://godoc.org/github.com/miekg/dns. It responds (with an empty body) to queries from kdig over tcp, (and udp is but a goroutine away). Next steps will be taking what I have now and forwarding the requests over tls to an external dns server.
 - Ran gofmt because I realised I had my indents all muddled up. (Still setting up my personal laptop for go development).
 - Initial commit with README, and a basic net.Listen and net.Dial that forwards packets, as verified by netcat listening on port 2411, and broadcasting packets from port 2410. Mostly just a way of playing around with new bits of go's standard library that I haven't used. Also experimented with pushing packets into it from kdig, to see what data might get dumped along the way. Lots of research and exploration at this stage, it is still nowhere near a DNS - DoT proxy, but it's been interesting pushing packets around. (Although only in one direction).
--- main.go
+++ main.go
@@ -1,34 +1,67 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
+  "log"
+  "net"
+  "os"
+  "os/signal"
+  "syscall"
+  "time"
 
-	"github.com/miekg/dns"
+  "github.com/miekg/dns"
 )
 
 func shutdownServer(s *dns.Server) {
-	err := s.Shutdown()
+  err := s.Shutdown()
 
-	if err != nil {
-		// log with fatal level here to really kill everything in case we have an error
-		log.Fatal("Failed to shutdown server %s", s.Net)
-	}
+  if err != nil {
+    // log with fatal level here to really kill everything in case we have an error
+    log.Fatal("Failed to shutdown server %s", s.Net)
+  }
+}
+
+func createHandler(client *dns.Client, conn *dns.Conn) func (dns.ResponseWriter, *dns.Msg) {
+  return func (w dns.ResponseWriter, m *dns.Msg) {
+    msgString := ""
+    for _, q := range m.Question {
+      msgString += q.String()
+    }
+    log.Printf("Received query: '%s'", msgString)
+    response, rtt, err := client.ExchangeWithConn(m, conn)
+    if err != nil {
+      log.Fatalf("Can't reach upstream\n%s", err)
+      return
+    }
+    log.Printf("Response Time: '%s'", rtt.String())
+    w.WriteMsg(response)
+  }
 }
 
 func main() {
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGTERM)
-	signal.Notify(signalChan, syscall.SIGINT)
+  signalChan := make(chan os.Signal, 1)
+  signal.Notify(signalChan, syscall.SIGTERM)
+  signal.Notify(signalChan, syscall.SIGINT)
 
-	tcpServer := &dns.Server{Addr: ":2410", Net: "tcp"}
+  c := new(dns.Client)
+  c.Net = "tcp-tls"
+  c.Dialer = &net.Dialer{
+    Timeout: 200 * time.Millisecond,
+  }
 
-	go tcpServer.ListenAndServe()
-	log.Println("Now listening")
+  conn, err := c.Dial("1.1.1.1:853")
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	sig := <-signalChan
-	log.Printf("Received signal: %q, shutting down..", sig.String())
-	shutdownServer(tcpServer)
+  tcpServer := &dns.Server{Addr: ":2410", Net: "tcp"}
+
+  dnsHandler := createHandler(c, conn)
+
+  go tcpServer.ListenAndServe()
+  dns.Handle(".", dns.HandlerFunc(dnsHandler))
+  log.Println("Now listening")
+
+  sig := <-signalChan
+  log.Printf("Received signal: %q, shutting down..", sig.String())
+  shutdownServer(tcpServer)
 }
